eeab044ce19e75d1591d3289271c2cd4d5fe9cd9
yonding <uwrgoy7584@khu.ac.kr>
2024-02-14 23:29:35 +0900

    내용 수정

--- src/scripts/store.js
+++ src/scripts/store.js
@@ -3,40 +3,41 @@ import {
 } from 'vue'
 
 export const store = reactive({
-    projects: [{
-            no: 1,
-            title: "도시락 배달 서비스, 벤토벤",
-            text: "KVP 창업 프로그램에서 작성한 린캔버스와 사업계획서",
-            imgPath: "src/assets/card_img/card_img_1.png", 
-            reg_date: "2023.3.13"
-        },
-        {
-            no:2,
-            title: "Apache Calcite's Adapter Architecture",
-            text: "Federated Query 성능을 개선하기 위한 어댑터 아키텍처",
-            imgPath: "../assets/card_img/card_img_2.png",
-            reg_date: "2023.3.13"
-        },
-        {
-            no:3,
-            title: "P2P와 WebRTC",
-            text: "WebRTC로 구현한 P2P 화상회의",
-            imgPath: "../assets/card_img/card_img_3.png",
-            tag: ["WebRTC"]
-        },
-        {
-            no:4,
-            title: "우산 대여 서비스, 물막이",
-            text: "지쿠터, 따릉이, 물막이 Let's Go",
-            imgPath: "../assets/card_img/card_img_4.png",
-            tag: ["AWS"]
-        },
-        {
-            no:5,
-            title: "모두 몰두할 시간",
-            text: "집중 관리 앱, 몰두 개발 과정",
-            imgPath: "../assets/card_img/card_img_5.png",
-            tag: ["moldoo"]
-        }
+    projects: [
+        // {
+        //     no: 1,
+        //     title: "도시락 배달 서비스, 벤토벤",
+        //     text: "KVP 창업 프로그램에서 작성한 린캔버스와 사업계획서",
+        //     imgPath: "src/assets/card_img/card_img_1.png", 
+        //     reg_date: "2023.3.13"
+        // },
+        // {
+        //     no:2,
+        //     title: "Apache Calcite's Adapter Architecture",
+        //     text: "Federated Query 성능을 개선하기 위한 어댑터 아키텍처",
+        //     imgPath: "../assets/card_img/card_img_2.png",
+        //     reg_date: "2023.3.13"
+        // },
+        // {
+        //     no:3,
+        //     title: "P2P와 WebRTC",
+        //     text: "WebRTC로 구현한 P2P 화상회의",
+        //     imgPath: "../assets/card_img/card_img_3.png",
+        //     tag: ["WebRTC"]
+        // },
+        // {
+        //     no:4,
+        //     title: "우산 대여 서비스, 물막이",
+        //     text: "지쿠터, 따릉이, 물막이 Let's Go",
+        //     imgPath: "../assets/card_img/card_img_4.png",
+        //     tag: ["AWS"]
+        // },
+        // {
+        //     no:5,
+        //     title: "모두 몰두할 시간",
+        //     text: "집중 관리 앱, 몰두 개발 과정",
+        //     imgPath: "../assets/card_img/card_img_5.png",
+        //     tag: ["moldoo"]
+        // }
     ]
 })
